e3be1ae617462107ea95f0748000728ec5d7dfc8
Justin W <s0181433@student.rockvalleycollege.edu>
2019-10-28 20:19:45 -0500

    Initial Commit

---
+++ MySettings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySettings
+//
+//  Created by Justin Williams on 10/28/19.
+//  Copyright © 2019 Justin Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
